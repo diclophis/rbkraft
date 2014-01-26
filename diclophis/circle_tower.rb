45ebd903493137023bcd15ef058157a247c65ece
diclophis <diclophis@gmail.com>
2014-01-26 03:53:16 +0000

    first floor

--- diclophis/circle_tower.rb
+++ diclophis/circle_tower.rb
@@ -3,7 +3,7 @@
 require './diclophis/diclophis_world_painter'
 
 ox = 10050
-oy = 63
+oy = 62
 #63 is water
 oz = -50
 
@@ -52,11 +52,15 @@ z = nil
 
 y = 0
 t = 0
-top_floor = 7
+
+floors = 1
+per_floor = 5
+
+top_floor = floors * per_floor
 arc_r = 37.0
 
 if false
-  ((top_floor)).times { |f|
+  ((top_floor + 1)).times { |f|
     y = top_floor - f
     max_arc.times { |d|
       a = (360 - d).to_f * (Math::PI / 180.to_f)
@@ -78,9 +82,11 @@ if false
       sleep 1
     end
   }
+
+  exit
 end
 
-arc_r = 30.0
+arc_r = 30
 y = 0
 
 floors.times { |f|
@@ -90,20 +96,21 @@ floors.times { |f|
 
     window_mod = 0
 
-    arc_r.to_i.times { |r|
-      x, z = painter.xy_from_angle_radius(a, r.to_f)
-   
-      floor_type = (((window_mod * 2) % 10) == 0) ? glow_type : type
-
-      if r > 9
-        blocks << [x.to_i, y.to_i, z.to_i, floor_type]
-      end
-
-      window_mod += 1
+    (arc_r).to_i.times { |r|
+      off_r = 0.0
+      1.times { |o|
+        x, z = painter.xy_from_angle_radius(a + off_r, r.to_f)
+        if r > 9
+          floor_type = ((((r + 2) * 1) % 5) == 0) ? glow_type : type
+          blocks << [x.round, y.round, z.round, floor_type]
+        end
+        off_r += 0.33
+      }
     }
 
-    x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 0.1)
-    blocks << [(x).to_i, y.to_i, z.to_i, type]
+=begin
+    x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 0.11)
+    blocks << [(x).to_i, y.to_i, z.to_i, glow_type]
 
     x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 0.33)
     blocks << [(x).to_i, y.to_i, z.to_i, type]
@@ -117,12 +124,25 @@ floors.times { |f|
     x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 1.0)
     blocks << [(x).to_i, y.to_i, z.to_i, type]
 
+    x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 1.33)
+    blocks << [(x).to_i, y.to_i, z.to_i, type]
+
+    x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 1.66)
+    blocks << [(x).to_i, y.to_i, z.to_i, type]
+
+    x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 2.0)
+    blocks << [(x).to_i, y.to_i, z.to_i, type]
+
+    x, z = painter.xy_from_angle_radius(a, arc_r.to_f + 2.25)
+    blocks << [(x).to_i, y.to_i, z.to_i, type]
+=end
+
     x, z = painter.xy_from_angle_radius(a, arc_r)
 
     ny = 0
     per_floor.times { |h|
-      should_be_glass = (((((d + t).to_f + (8.0)).to_f / (18).to_f)).to_i % 2)
-      wall_type = (should_be_glass == 0) ? water_type : type
+      should_be_glass = (((((d + t).to_f + (-0.1)).to_f / (18).to_f)).round % 2)
+      wall_type = (should_be_glass == 0) ? air_type : type
       blocks << [(x).to_i, y.to_i + (h + 1), z.to_i, wall_type]
       ny = y.to_i + (h + 1)
     }
@@ -137,7 +157,7 @@ x = 0
 oz = 0
 arc_r = 5.0
 
-max_arc = (360 * 5) + 4
+max_arc = (360 * 5) + 23
 
 max_arc.times { |d|
   a = d.to_f * (Math::PI / 180.to_f)
@@ -147,7 +167,7 @@ max_arc.times { |d|
 
     s = 8.0
     y = (((d % 360).to_f / 360.to_f) * s).to_i + ((d / 360) * s)
-    ny = ((((d + 17) % 360).to_f / 360.to_f) * s).to_i + ((d / 360) * s)
+    ny = ((((d + 25) % 360).to_f / 360.to_f) * s).to_i + ((d / 360) * s)
 
     step_type = (y == ny) ? slab : type
 
@@ -157,10 +177,10 @@ max_arc.times { |d|
 
 blocks.uniq!
 
-blocks.sort_by { |b| b[1] }.reverse.each_with_index { |b, i|
+blocks.sort_by { |b| b[2] }.each_with_index { |b, i|
   painter.place(*b)
-  if (i % 4000) == 0
+  if (i % 425) == 0
     puts "wtf"
-    sleep 1
+    sleep 0.75
   end
 }
