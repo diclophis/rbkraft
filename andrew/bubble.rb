79f68bf9a2b9317031fc8f4538c6b237c4ec4ad9
Andrew Cantino <cantino@gmail.com>
2014-08-30 00:23:39 -0700

    bubble tunnel

--- andrew/bubble.rb
+++ andrew/bubble.rb
@@ -8,32 +8,45 @@ player_name = gets.strip
 
 painter.center = painter.player_position(player_name)
 
-size = 3
 memory = {}
 previous_area_tiles = []
+previous_wall_tiles = []
 previous_position = nil
 while true
   sleep 0.05
   new_pos = painter.player_position(player_name) - painter.center
   if new_pos != previous_position
     area_tiles = []
+    wall_tiles = []
+
+    size = 4
+
     (-size).upto(size) do |x|
       (-size).upto(size) do |y|
         (-size).upto(size) do |z|
-          area_tiles << new_pos + Vector.new(x, y, z)
+          if x == size || x == -size || y == size || y == -size || z == size || z == -size
+            wall_tiles << new_pos + Vector.new(x, y, z)
+          else
+            area_tiles << new_pos + Vector.new(x, y, z)
+          end
         end
       end
     end
 
-    painter.bulk_test(area_tiles - memory.keys).each do |tile, type|
+    painter.bulk_test((area_tiles + wall_tiles) - memory.keys).each do |tile, type|
       memory[tile] = type
     end
 
     painter.async do
       (area_tiles - previous_area_tiles).each do |tile|
-        painter.place tile, 'air' unless memory[tile] == 'air'
+        painter.place tile, 'air' unless memory[tile] == 'air' && !previous_wall_tiles.include?(tile)
       end
 
+      (wall_tiles - previous_wall_tiles).each do |tile|
+        painter.place tile, 'glass'
+      end
+
+      # Glowstone
       if previous_position
         old_glow_stone = Vector.new(previous_position.x + 2, previous_position.y + 2, previous_position.z + 2)
         if area_tiles.include?(old_glow_stone)
@@ -44,17 +57,23 @@ while true
       end
       painter.place(Vector.new(new_pos.x + 2, new_pos.y + 2, new_pos.z + 2), 'glowstone')
 
-      (previous_area_tiles - area_tiles).each do |tile|
+      ((previous_area_tiles + previous_wall_tiles) - (area_tiles + wall_tiles)).each do |tile|
         if WorldPainter::TYPE_MAPPINGS[memory[tile]]
-          painter.place tile, WorldPainter::TYPE_MAPPINGS[memory[tile]] unless memory[tile] == 'air'
+          replacement = WorldPainter::TYPE_MAPPINGS[memory[tile]]
+          if replacement == 'glass'
+            painter.place tile, 'air'
+          else
+            painter.place tile, replacement unless memory[tile] == 'air' && !previous_wall_tiles.include?(tile)
+          end
         else
           puts "UNKNOWN: #{memory[tile]}"
         end
-        memory.delete(tile)
+        # memory.delete(tile)
       end
     end
 
     previous_area_tiles = area_tiles
+    previous_wall_tiles = wall_tiles
     previous_position = new_pos
   end
 end
