72bd0f5a08fce2b649e3991d464fd9d83d748ecf
mavenlink <jon@mavenlink.com>
2015-06-12 21:14:38 -0700

    add blocker to script

--- ansible/roles/minecraft/files/minecraft.sh
+++ ansible/roles/minecraft/files/minecraft.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
 set -e
+set -x
 
 RAM=1500M
 MINECRAFT_JAR=/root/minecraft.jar
 MINECRAFT_ROOT=/opt/minecraft
-MAVENCRAFT_WRAPPER=/root/mavencraft/minecraft-wrapper/server.rb
-#MAVENCRAFT_WRAPPER_SOCK=0.0.0.0:25566
-#/tmp/minecraft-wrapper.sock
-export MAVENCRAFT_WRAPPER_PORT=25566
+MAVENCRAFT_WRAPPER=/root/mavencraft/minecraft-wrapper/server2.rb
+MAVENCRAFT_BLOCKER=/root/mavencraft/minecraft-wrapper/blocker.rb
 
 mkdir -p $MINECRAFT_ROOT
 cd $MINECRAFT_ROOT
 
-#touch $MAVENCRAFT_WRAPPER_SOCK
-#rm $MAVENCRAFT_WRAPPER_SOCK
-
-ruby $MAVENCRAFT_WRAPPER java -d64 -XX:UseSSE=2 -Xmx$RAM -Xms$RAM -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -server -jar $MINECRAFT_JAR nogui
+ruby $MAVENCRAFT_WRAPPER $MAVENCRAFT_BLOCKER java -d64 -XX:UseSSE=2 -Xmx$RAM -Xms$RAM -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -server -jar $MINECRAFT_JAR nogui
