da093dc45e7b730bb5dc5fdd22815fe4a41aa9e1
mavenlink <jon@mavenlink.com>
2015-06-13 01:11:47 -0700

    checkpoint

--- diclophis/big_pyramid.rb
+++ diclophis/big_pyramid.rb
@@ -5,22 +5,23 @@ $: << "diclophis"
 
 require 'diclophis_world_painter'
 
-def water_or_tnt(painter)
+srand
+
+def water_or_tnt(i, painter)
   #if rand > 0.9
   #  painter.lava_type
   #elsif rand > 0.9
   #  painter.water_type
   #else
+  if ((i % 64) % 128) == 0
+    painter.glow_type
+  else
     painter.sandstone_type
-  #end
+  end
 end
 
 def mostly_air(painter)
-  if rand > 0.5
-    painter.air_type
-  else
-    painter.sand_type
-  end
+  painter.air_type
 end
 
 painter = DiclophisWorldPainter.new(24000, 71, 24000)
@@ -28,21 +29,24 @@ position = painter.player_position("diclophis")
 puts position.inspect
 
 s = 64
+i = 0
 
 painter.async do
   s.times { |x|
     s.times { |y|
       s.times { |z|
         #if ((x % 16) > 8) || ((y % 8) > 4) || ((z % 4) > 2)
-        if ((x % 16) > 8) || ((y % 8) > 4) || ((z % 4) > 2)
+        if ((x % 16) > 8) || ((y % 8) > 2) || ((z % 4) > 2)
           if ((x % 9) > 7)
-            painter.place(x, y - 8, z, water_or_tnt(painter))
+            i += 1
+            painter.place(x, y - 8, z, water_or_tnt(i, painter))
           else
             painter.place(x, y - 8, z, mostly_air(painter))
           end
         else
           if ((z % 11) > 9)
-            painter.place(x, y - 8, z, water_or_tnt(painter))
+            i += 1
+            painter.place(x, y - 8, z, water_or_tnt(i, painter))
           else
             painter.place(x, y - 8, z, mostly_air(painter))
           end
