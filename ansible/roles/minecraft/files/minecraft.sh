ae97b5bef18362f52ca77e1076f26255b0231d5e
mavenlink <jon@mavenlink.com>
2015-06-22 22:59:05 -0700

    really stress test the instance

--- ansible/roles/minecraft/files/minecraft.sh
+++ ansible/roles/minecraft/files/minecraft.sh
@@ -32,7 +32,7 @@ rm -Rf /home/mavencraft/world*
 rm -f /tmp/dynasty.sock
 
 echo starting-minecraft
-ruby $MAVENCRAFT_WRAPPER ruby $MAVENCRAFT_BLOCKER java -d64 -XX:UseSSE=2 -Xmx$RAM -Xms$RAM -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -server -jar $MINECRAFT_ROOT/minecraft.jar nogui &
+ruby $MAVENCRAFT_WRAPPER ruby $MAVENCRAFT_BLOCKER java -d64 -XX:UseSSE=2 -Xmx$RAM -Xms$RAM -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -server -jar $MINECRAFT_ROOT/minecraft.jar nogui &
 
 echo starting-overviewer
 sudo -u mavencraft sh /home/mavencraft/mavencraft/scripts/overviewer.sh &
