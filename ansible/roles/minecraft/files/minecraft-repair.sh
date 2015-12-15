9381699f1be2fb60618616d6511311350faf7401
Ubuntu <ubuntu@mavencraft.net>
2015-12-15 06:22:37 +0000

    better run scripts

--- ansible/roles/minecraft/files/minecraft-repair.sh
+++ ansible/roles/minecraft/files/minecraft-repair.sh
@@ -9,9 +9,17 @@ if [ "$line" = "destroy" ];
 then
   echo destroying
 
+  echo -e 'authentic\nsave-on\nsave-all' | nc -w 1 localhost 25566 2>&1 > /dev/null
+  echo -e 'authentic\nsave-on\nsave-all' | nc -w 1 localhost 25567 2>&1 > /dev/null
+  echo -e 'authentic\nsave-on\nsave-all' | nc -w 1 localhost 25566 2>&1 > /dev/null
+  echo -e 'authentic\nsave-on\nsave-all' | nc -w 1 localhost 25567 2>&1 > /dev/null
+
+  pkill -9 -f overviewer || true
+  pkill -9 -f mapcrafter || true
+  pkill -9 -f 'java(.*)mavencraft-' || true
+  pkill -9 -f 'server2(.*)mavencraft-' || true
+
   rm -Rf /home/mavencraft/mavencraft-world
-  rm -Rf /usr/share/nginx/html/normal
-  rm -Rf /usr/share/nginx/html/scenes
 
   line="kill"
 fi;
@@ -26,9 +34,7 @@ then
   echo -e 'authentic\nsave-on\nsave-all' | nc -w 1 localhost 25567 2>&1 > /dev/null
 
   pkill -9 -f overviewer || true
-  #pkill -9 -f java || true
-  #pkill -9 -f java || true
-  #pkill -9 -f ruby || true
+  pkill -9 -f mapcrafter || true
   pkill -9 -f 'java(.*)mavencraft-' || true
   pkill -9 -f 'server2(.*)mavencraft-' || true
 
