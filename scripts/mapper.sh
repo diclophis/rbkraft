75d31fccfce523fe647e7c5f330ba767cb153e84
mavenlink <diclophis@gmail.com>
2015-12-14 22:33:19 -0800

    add mapcrafter

--- scripts/mapper.sh
+++ scripts/mapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/ubuntu/mapcrafter/build/src/mapcrafter -b --logging-config /home/ubuntu/mapcrafter/build/mapcrafter-log.conf -c /home/ubuntu/mapcrafter/build/mapcrafter.conf -j 16 2>&1 >/dev/null | logger -t mapcrafter
+/home/ubuntu/mapcrafter/build/src/mapcrafter -b --logging-config /home/mavencraft/mapcrafter-log.conf -c /home/mavencraft/mapcrafter.conf -j 4 2>&1 >/dev/null | logger -t mapcrafter
