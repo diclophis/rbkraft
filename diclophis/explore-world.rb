274586ac771294bd9d7a33389fa439bc17398304
Jon Bardin <diclophis@gmail.com>
2016-01-03 21:18:00 -0800

    local build

--- diclophis/explore-world.rb
+++ diclophis/explore-world.rb
@@ -10,21 +10,33 @@ srand
 oox = 0
 ooy = 0
 ooz = 0
-s = 128
+s = 40
 
-global_painter = DiclophisWorldPainter.new(oox, ooy, ooz)
+global_painter = DiclophisWorldPainter.new(true, oox, ooy, ooz)
 puts "connected"
 
 global_painter.async do
   (-s..s).each { |ttx|
     (-s..s).each { |tty|
-      v = 16
-      global_painter.place(ttx * v, 1, tty * v, global_painter.bedrock_type)
-      global_painter.place((ttx * v) + 8, 1, (tty * v) + 8, global_painter.bedrock_type)
-      global_painter.place(ttx * v, 1, tty * v, global_painter.air_type)
-      global_painter.place((ttx * v) + 8, 1, (tty * v) + 8, global_painter.air_type)
-      $stdout.write(".")
-      sleep 0.01
+      v = 8
+      z = 1
+      (-z..z).each { |ttzx|
+      (-z..z).each { |ttzy|
+      #global_painter.place(ttx * v, 1, tty * v, global_painter.bedrock_type)
+      global_painter.place(ttx * v, 120, tty * v, global_painter.air_type)
+      #global_painter.place(ttx * v, 1, tty * v, global_painter.bedrock_type)
+      #global_painter.place((ttx * v) + ttzx, 1, (tty * v) + ttzy, global_painter.bedrock_type)
+      #global_painter.place((ttx * v) + 8, 1, (tty * v) + 8, global_painter.air_type)
+      #global_painter.place((ttx * v) - 8, 1, (tty * v) - 8, global_painter.bedrock_type)
+      #global_painter.place((ttx * v) - 8, 1, (tty * v) - 8, global_painter.air_type)
+      #global_painter.place((ttx * v) - 8, 1, (tty * v) + 8, global_painter.bedrock_type)
+      #global_painter.place((ttx * v) - 8, 1, (tty * v) + 8, global_painter.air_type)
+      #global_painter.place((ttx * v) - 8, 1, (tty * v) + 8, global_painter.bedrock_type)
+      #global_painter.place((ttx * v) - 8, 1, (tty * v) + 8, global_painter.air_type)
+      #$stdout.write(".")
+      sleep 0.0000001
+      }
+      }
     }
   }
 end
--- scripts/mapper.sh
+++ scripts/mapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/ubuntu/mapcrafter/build/src/mapcrafter -b --logging-config /home/mavencraft/mapcrafter-log.conf -c /home/mavencraft/mapcrafter.conf -j 4 2>&1 >/dev/null | logger -t mapcrafter
+mapcrafter -b --logging-config /home/mavencraft/mapcrafter-log.conf -c /home/mavencraft/mapcrafter.conf -j 4 2>&1 >/dev/null | logger -t mapcrafter
