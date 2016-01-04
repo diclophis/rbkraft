d3bbddf27a4f3d5fd496721995a3df299df1d1c1
Ubuntu <ubuntu@mavencraft.net>
2016-01-04 06:24:53 +0000

    better read chunk

--- scripts/mapper.sh
+++ scripts/mapper.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/home/ubuntu/mapcrafter/build/src/mapcrafter -b --logging-config /home/mavencraft/mapcrafter-log.conf -c /home/mavencraft/mapcrafter.conf -j 4 2>&1 >/dev/null | logger -t mapcrafter
+/home/ubuntu/mapcrafter/build/src/mapcrafter -b --logging-config /home/mavencraft/mapcrafter-log.conf -c /home/mavencraft/mapcrafter.conf -j 15 2>&1 >/dev/null | logger -t mapcrafter
+echo $? | logger  -t mapcrafter
+sleep 3
