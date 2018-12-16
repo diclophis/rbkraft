67c78ab39273b50df9d33f19039eda333651e4e0
Jon Bardin <diclophis@gmail.com>
2018-12-15 18:30:49 -0800

    remove voxsurf

--- setup-debug.sh
+++ setup-debug.sh
@@ -3,32 +3,6 @@
 set -e
 set -x
 
-#cd /home/minecraft
-#git clone https://github.com/sylefeb/VoxSurf.git
-#
-#cd /home/minecraft/VoxSurf
-#git checkout master
-#
-#git config --file=.gitmodules submodule.LibSL-small.url https://github.com/sylefeb/LibSL-small.git
-#git submodule sync
-#git submodule update --init --recursive --remote
-#
-##git submodule init && git submodule update
-#
-#ls -la
-#
-#ls -la LibSL-small
-#
-##echo '#define SRC_PATH "/var/tmp"' > path.h
-##g++ -ILibSL-small -ILibSL-small/LibSL main.cpp -o voxsurf
-#
-#cp /var/tmp/voxsurf-main.cpp main.cpp
-#
-#cmake .
-#make -j
-#
-#ls -la
-
 cd /home/minecraft/fcl
 
 mkdir build
