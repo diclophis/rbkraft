8a6bb70250e1300767579ce3eb47e382feef7c08
Jon Bardin <diclophis@gmail.com>
2019-12-10 03:39:43 -0500

    the cuboid has landed

--- demo.sh
+++ demo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-#bash run-gen-text.sh "Hello World" | bash run-vox-blit.sh 1024 2000 64 2000 sandstone
+bash run-gen-text.sh "mavencraft.net" | bash run-vox-blit.sh 1024 2000 48 2000 glowstone
 #bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/simple-structures.es | bash run-vox-blit.sh 256 -1000 0 -1000 sandstone
-bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/frames.es | bash run-vox-blit.sh 32 0 -96 0 sandstone
+#bash run-gen-scad.sh openscad/sphere.scad | bash run-vox-blit.sh 100 0 -96 0 air
+#bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/frames.es | bash run-vox-blit.sh 32 0 -96 0 sandstone
 #bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/pyramid.es | bash run-vox-blit.sh 72 600 -72 600 sandstone
 #bash run-gen-synth.sh openscad/structure-synth-alt.mlx eisenscript/other-buildings.es | bash run-vox-blit.sh 128 -2500 -32 -2500 sandstone
-#bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/octo.es | bash run-vox-blit.sh 600 0 -175 0 air
-#bash run-gen-scad.sh openscad/sphere.scad | bash run-vox-blit.sh 100 0 -96 0 air
+bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/octo.es | bash run-vox-blit.sh 600 0 -175 0 air
+bash run-gen-scad.sh openscad/sphere.scad | bash run-vox-blit.sh 72 0 -96 0 air
+bash run-gen-synth.sh openscad/structure-synth.mlx eisenscript/frames.es | bash run-vox-blit.sh 50 0 -96 0 obsidian
--- eisenscript/frames.es
+++ eisenscript/frames.es
@@ -1,26 +1,26 @@
 set background white
 
-{ hue 20 sat 0.8  } r2 
+{ hue 20 sat 0.8  } r2
 
-rule r2 maxdepth 5 {
-{ s 0.5  rz 33  b 0.9   }  r2
+rule r2 maxdepth 50 {
+{ s 0.66  rz 33  b 0.999   }  r2
 frame
 }
 
 rule frame  {
-{ s 0.1 1.1 0.1 x 5  z 5 } box
-{ s 0.1 1.1 0.1 x 5  z -5 } box
-{ s 0.1 1.1 0.1 x -5  z 5 } box
-{ s 0.1 1.1 0.1 x -5  z -5 } box
+{ s 0.01 1 0.01 x 50  z 50 } box
+{ s 0.01 1 0.01 x 50  z -50 } box
+{ s 0.01 1 0.01 x -50  z 50 } box
+{ s 0.01 1 0.01 x -50  z -50 } box
 
-{ s 1 0.1 0.1 y 5  z 5 } box
-{ s 1 0.1 0.1 y 5  z -5 } box
-{ s 1 0.1 0.1 y -5  z 5 } box
-{ s 1 0.1 0.1 y -5  z -5 } box
+{ s 1 0.01 0.01 y 50  z 50 } box
+{ s 1 0.01 0.01 y 50  z -50 } box
+{ s 1 0.01 0.01 y -50  z 50 } box
+{ s 1 0.01 0.01 y -50  z -50 } box
 
-{ s 0.1 0.1 1 y 5  x 5 } box
-{ s 0.1 0.1 1 y 5  x -5 } box
-{ s 0.1 0.1 1 y -5  x 5 } box
-{ s 0.1 0.1 1 y -5  x -5 } box
+{ s 0.01 0.01 1 y 50  x 50 } box
+{ s 0.01 0.01 1 y 50  x -50 } box
+{ s 0.01 0.01 1 y -50  x 50 } box
+{ s 0.01 0.01 1 y -50  x -50 } box
 }
 
