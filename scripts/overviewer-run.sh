c32b9f185c2ed410570ed01676e65802b31deacd
Jon Bardin <diclophis@gmail.com>
2020-03-14 21:29:51 -0400

    add a console pod for emitting mavencraft commands

--- scripts/overviewer-run.sh
+++ scripts/overviewer-run.sh
@@ -3,13 +3,14 @@
 # makes map and invokes film
 
 #set -e
-set -x
+#set -x
 
 MINECRAFT_ROOT=/home/app
 
 while true;
 do
-  sleep 3
+  sleep 5
+
   if [ -e $MINECRAFT_ROOT/world/level.dat ];
   then
     cat $MINECRAFT_ROOT/scripts/normal-save.cmd | nc -w 1 localhost 25566 2>&1 > /dev/null
@@ -17,6 +18,6 @@ do
     $MINECRAFT_ROOT/scripts/mapper.sh $1
   else
     cat $MINECRAFT_ROOT/scripts/initial-save.cmd | nc -w 1 localhost 25566 2>&1 > /dev/null
-    (test -e $MINECRAFT_ROOT/world/session.lock && inotifywait -t 6 -e CLOSE $MINECRAFT_ROOT/world/session.lock) # || sleep 1
+    (test -e $MINECRAFT_ROOT/world/session.lock && inotifywait -t 30 -e CLOSE $MINECRAFT_ROOT/world/session.lock) # || sleep 1
   fi
 done
