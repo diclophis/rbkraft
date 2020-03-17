9cafd265ad21f82135c6cc46b677052813b9ddca
Jon Bardin <diclophis@gmail.com>
2020-03-17 03:14:44 -0400

    better console interface

--- diclophis/scad.rb
+++ diclophis/scad.rb
@@ -6,10 +6,14 @@ $: << "diclophis"
 require 'tempfile'
 require 'diclophis_world_painter'
 
-SCAD=ARGV[0]
+inp = $stdin.read
+
+tmp_scad = Tempfile.new
+tmp_scad.write(inp)
+tmp_scad.flush
 
 tmp_stl = Tempfile.new(["rendered-es", ".stl"])
-system("openscad", "--autocenter", "-o", tmp_stl.path, SCAD) || exit(1)
+system("openscad", "--autocenter", "-o", tmp_stl.path, tmp_scad.path) || exit(1)
 tmp_stl.rewind
 
 $stdout.write(tmp_stl.read)
